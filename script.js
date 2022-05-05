8f402806a21e1c4f8ea58323913dab7510488ff4
Trenton <050trenton@gmail.com>
2022-05-05 15:11:12 -0500

    adding the values in js

--- script.js
+++ script.js
@@ -10,5 +10,27 @@ const countriesContainer = document.querySelector('.countries');
 
 // Older way to call. 
 const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v2/name/portal');
+request.open('GET', 'https://restcountries.com/v2/name/portugal');
 request.send();
+
+request.addEventListener('load', function() {
+    // Gets the data from the API and then put it in the console
+    console.log(this.responseText);
+
+    // Makes the data more clear 
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `
+    <article class="country">
+      <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+    </article>
+    `
+})
