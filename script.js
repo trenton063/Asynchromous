3975402394be7bdeb40881fdde498a6183ca4ac2
Trenton <050trenton@gmail.com>
2022-05-05 14:56:42 -0500

    Get and call to API

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 'use strict';
 
+// https://restcountries.com/v2/ 
+// change of url use this for countries
+
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+// Older way to call. 
+const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.com/v2/name/portal');
+request.send();
